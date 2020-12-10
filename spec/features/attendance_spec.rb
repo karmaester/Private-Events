2b527ccd679401ea32670914642211dec53c9f88
Khristian Rojas <khristianrojas15@gmail.com>
2020-12-10 19:10:20 -0300

    Add unit test for attendance model

---
+++ spec/features/attendance_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Attendance, type: :model do
+    
+    it 'is not valid if user id is not provided' do
+        user = User.create(username: 'thisisatest', email: 'test@t.com')
+        event = Event.create(name: 'Event', user_id: user.id)
+        expect(Attendance.new(event_id: event.id)).to_not be_valid
+    end
+
+    it 'is not valid if event id is not provided' do
+        user = User.create(username: 'thisisatest', email: 'test@t.com')
+        expect(Attendance.new(user_id: user.id)).to_not be_valid
+    end
+    
+    it 'is valid if event and user id are provided' do
+        user = User.create(username: 'thisisatest', email: 'test@t.com')
+        event = Event.create(name: 'Event', user_id: user.id)
+        expect(Attendance.new(event_id: event.id, user_id: user.id)).to be_valid
+    end
+end
