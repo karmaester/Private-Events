a9cca14c524411106cfe10d8315f4399129e36ff
Khristian Rojas <khristianrojas15@gmail.com>
2020-12-11 17:27:26 -0300

    Rubocop fixes

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -24,18 +24,18 @@ class AttendancesController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
     @event.attendees << current_user
-      if @event.save
-        redirect_to root_path
-        flash[:notice] = 'Your are checked in to this Event'
-      else
-        format.html { render :new }
-        format.json { render json: @attendance.errors, status: :unprocessable_entity }
-      end
+    if @event.save
+      redirect_to root_path
+      flash[:notice] = 'Your are checked in to this Event'
+    else
+      format.html { render :new }
+      format.json { render json: @attendance.errors, status: :unprocessable_entity }
+    end
   end
-
 
   # PATCH/PUT /attendances/1
   # PATCH/PUT /attendances/1.json
+  
   def update
     respond_to do |format|
       if @attendance.update(attendance_params)
